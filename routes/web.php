5c2085f3a1ef9adcf3a9a0b44fc784179620095e
Nikita Ilyukov <nilyukov@offsiteteam.com>
2021-01-18 20:00:38 +0300

    Get layouts

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/categories', function () {
+    return view('categories');
+});
+
+Route::get('/mobiles/iphone_x_64', function () {
+    return view('product');
+});
+
+
